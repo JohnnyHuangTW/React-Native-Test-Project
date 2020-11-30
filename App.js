6beb563bf812cf0eed0011a68059b7bec3d68c1f
Johnny Huang <jtk155586@gmail.com>
2020-11-30 17:20:52 +0800

    restructure and add SafeAreaView

--- App.js
+++ App.js
@@ -1,27 +1,38 @@
-import { StatusBar } from 'expo-status-bar'
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
+import { Text } from 'react-native'
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+import { NavigationContainer } from '@react-navigation/native'
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs'
+import { createStackNavigator } from '@react-navigation/stack'
 import * as Device from 'expo-device'
 
 // components
-import Footer from './Components/Footer'
+import TabMenu from './Components/TapMenu'
 
-export default function App() {
+const Demo = () => {
   return (
-    <View style={styles.container}>
-      <Text>
-        {Device.manufacturer} : {Device.modelName}
-      </Text>
-      <Footer />
-    </View>
+    <SafeAreaView style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }}>
+      <Text>This is top text.</Text>
+      <Text>This is bottom text.</Text>
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  }
-})
+const Stack = createStackNavigator()
+const Tab = createBottomTabNavigator()
+
+export default function App() {
+  return (
+    <SafeAreaProvider>
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Tab1" tabBar={props => <TabMenu {...props} />}>
+          <Tab.Screen name="Tab1" component={Demo} />
+          <Tab.Screen name="Tab2" component={Demo} />
+          <Tab.Screen name="Tab3" component={Demo} />
+          <Tab.Screen name="Tab4" component={Demo} />
+          <Tab.Screen name="Tab5" component={Demo} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaProvider>
+  )
+}
---
+++ Components/TapMenu.js
@@ -0,0 +1,95 @@
+import React from 'react'
+import styled from 'styled-components/native'
+
+const Wrapper = styled.View`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  justify-content: space-evenly;
+  align-items: flex-end;
+  height: 60px;
+`
+
+const StyledTouchableOpacity = styled.TouchableOpacity`
+  z-index: 10;
+  height: 60px;
+  line-height: 60px;
+  text-align: center;
+  background-color: #424242;
+  flex: 1;
+  ${({ isFocused }) =>
+    isFocused &&
+    `
+      flex: 1.5;
+      height: 80px;
+      line-height: 80px;
+      border-top-left-radius: 12px;
+      border-top-right-radius: 12px;
+    `}
+`
+const StyledText = styled.Text`
+  font-size: 18px;
+  align-self: stretch;
+  color: white;
+  font-weight: bold;
+  line-height: ${({ isFocused }) => (isFocused ? '80px' : '60px')};
+`
+
+const TapMenu = ({ state, descriptors, navigation }) => {
+  const focusedOptions = descriptors[state.routes[state.index].key].options
+
+  if (focusedOptions.tabBarVisible === false) {
+    return null
+  }
+
+  return (
+    <Wrapper>
+      {state.routes.map((route, index) => {
+        const { options } = descriptors[route.key]
+        const label =
+          options.tabBarLabel !== undefined
+            ? options.tabBarLabel
+            : options.title !== undefined
+            ? options.title
+            : route.name
+
+        const isFocused = state.index === index
+
+        const onPress = () => {
+          const event = navigation.emit({
+            type: 'tabPress',
+            target: route.key,
+            canPreventDefault: true
+          })
+
+          if (!isFocused && !event.defaultPrevented) {
+            navigation.navigate(route.name)
+          }
+        }
+
+        const onLongPress = () => {
+          navigation.emit({
+            type: 'tabLongPress',
+            target: route.key
+          })
+        }
+
+        return (
+          <StyledTouchableOpacity
+            accessibilityRole="button"
+            accessibilityState={isFocused ? { selected: true } : {}}
+            accessibilityLabel={options.tabBarAccessibilityLabel}
+            testID={options.tabBarTestID}
+            onPress={onPress}
+            onLongPress={onLongPress}
+            isFocused={isFocused}
+          >
+            <StyledText>{label}</StyledText>
+          </StyledTouchableOpacity>
+        )
+      })}
+    </Wrapper>
+  )
+}
+
+export default TapMenu
